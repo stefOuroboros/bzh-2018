fe77be57a79ee5c8e69ab9e4799b3ecb27cc50f6
stefOuroboros <stephane.gallois89@gmail.com>
2018-11-28 13:39:04 +0100

    Menu 1 opérationnel, menu 2 opérationnel et menu 99 operationnel

--- ihm.js
+++ ihm.js
@@ -5,32 +5,33 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 exports.start = function () {
-    rl.question(`*************************
-1. Rafraichir les données
-2. Lister les sessions
-99. Quitter`,
-        function (saisie) {
-            var service = require('./service');
-            switch (saisie) {
-                case '1':
-                    service.init(function (nb) {
-                        console.log(`... Données mises à jour. \n[init] : ${nb} sessions trouvées.`);
-                    });
 
-                    break;
-                case '2':
-                    service.listerSessions(function (nb) {
-                        console.log(`Yo ${nb}`);
-                        
-                    });
+    var texte = `*************************
+    1. Rafraichir les données
+    2. Lister les sessions
+    99. Quitter\n`;
 
-                    break;
-                case '99':
-                    rl.close();
-                    break;
-                default :
-                    break;
-            }
+    var menu = function (saisie) {
+        var service = require('./service');
+        switch (saisie) {
+            case '1':
+                service.init(function (nb) {
+                    console.log(`... Données mises à jour. \n[init] : ${nb} sessions trouvées.\n`);
+                    rl.question(texte, menu);
+                });
+                break;
+            case '2':
+                service.listerSessions(function (nb) {
+                    console.log(`Yo ${nb}`);
+                    rl.question(texte, menu);
+                });
+                break;
+            case '99':
+                rl.close();
+                break;
+            default:
+                rl.question(texte, menu);
         }
-    );
+    }
+    rl.question(texte, menu);
 };
--- service.js
+++ service.js
@@ -1,14 +1,18 @@
 var talks = [];
 var request = require('request');
-exports.init = function compterSessions (callback) {
-    
+exports.init = function compterSessions(callback) {
 
-    request('http://2018.breizhcamp.org/json/talks.json', {json: true}, function (err, res, tabTalks1) {
+
+    request('http://2018.breizhcamp.org/json/talks.json', {
+        json: true
+    }, function (err, res, tabTalks1) {
         if (err) {
             return console.log('Erreur', err);
         }
         talks = talks.concat(tabTalks1);
-        request('http://2018.breizhcamp.org/json/others.json', {json: true}, function (err, res, tabTalks2) {
+        request('http://2018.breizhcamp.org/json/others.json', {
+            json: true
+        }, function (err, res, tabTalks2) {
             if (err) {
                 return console.log('Erreur2', err);
             }
@@ -18,12 +22,22 @@ exports.init = function compterSessions (callback) {
     });
 };
 
-exports.listerSessions = function listerSessions(callback) {
-    request('http://2018.breizhcamp.org/json/talks.json', {json: true}, function (err, res, tabTalks1) {
+exports.listerSessions = function (callback) {
+    request('http://2018.breizhcamp.org/json/talks.json', {
+        json: true
+    }, function (err, res, tabTalks1) {
         if (err) {
             return console.log('Erreur', err);
         }
         talks = talks.concat(tabTalks1);
-        talks.forEach(talk=> console.log(`Conférence ${talk.venue_id} :  ${talk.name}`));        
+        request('http://2018.breizhcamp.org/json/others.json', {
+            json: true
+        }, function (err, res, tabTalks2) {
+            if (err) {
+                return console.log('Erreur2', err);
+            }
+            talks = talks.concat(tabTalks2);
+            talks.forEach(talk => console.log(`Conférence ${talk.venue_id} :  ${talk.name}`));
+        });
     });
 };
