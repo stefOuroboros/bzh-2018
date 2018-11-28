c2d234cc23b7f9962758231a692a29d5689b1b86
stefOuroboros <stephane.gallois89@gmail.com>
2018-11-28 11:28:34 +0100

    Données des sessions récupérées

--- service.js
+++ service.js
@@ -1,18 +1,19 @@
 var talks = [];
 
 exports.init = function (callback) {
+    var request = require('request');
 
-
-    var request = require('request')
-    console.log(request);
-    
-
-    request('http://2018.breizhcamp.org/json/others.json', { json: true }, function(err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-    });
-
-    request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function(err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-    });
-    callback(talks.length);
-};
+    request('http://2018.breizhcamp.org/json/talks.json', {json: true}, function (err, res, tabTalks1) {
+        if (err) {
+            return console.log('Erreur', err);
+        }
+        talks = talks.concat(tabTalks1);
+        request('http://2018.breizhcamp.org/json/others.json', {json: true}, function (err, res, tabTalks2) {
+            if (err) {
+                return console.log('Erreur2', err);
+            }
+            talks = talks.concat(tabTalks2)
+            callback(talks.length)
+        })
+    })
+}
